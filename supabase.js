22296f69c1fb8ff70dcf4d205407f179ece68474
Athrey <163659802+rustysuperbad@users.noreply.github.com>
2026-01-20 20:57:16 +0530

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://agmchftvtxmzfvnvhdon.supabase.co",
+  "sb_publishable_voYxd08fOABtJPNDBO5b8A_-xWJZNes"
+);
