71b4cda5f7e1b4d1f10aebc3afcfcc128133ed42
Athrey <163659802+rustysuperbad@users.noreply.github.com>
2026-01-26 18:59:53 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,13 @@
 // frontend/supabase.js
+// supabase.js
 
 const SUPABASE_URL = "https://agmchftvtxmzfvnvhdon.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_voYxd08fOABtJPNDBO5b8A_-xWJZNes";
 
-const supabase = window.supabase.createClient(
+const { createClient } = window.supabase;
+
+window.supabaseClient = createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
+
